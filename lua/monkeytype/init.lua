c20c5b07343e725c9c33497bcf2e19242ea64dbf
Martin <martinmelody.work@gmail.com>
2024-11-25 19:26:00 +0000

    commit

--- lua/monkeytype/init.lua
+++ lua/monkeytype/init.lua
@@ -1,14 +1,3 @@
-local M = {}
-
-M.config = {
-	quotes_file = vim.fn.stdpath("data") .. "/monkeytype-nvim/quotes.json",
-	test_duration = 60, -- Typing test duration in seconds
-	keymaps = { -- Default keymaps
-		start_test = { mode = "n", lhs = "<leader>tt", desc = "Start Typing Test" },
-		show_stats = { mode = "n", lhs = "<leader>ts", desc = "Show Typing Stats" },
-	},
-}
-
 -- Path to the default quotes file
 M.default_quotes_file = debug.getinfo(1).source:match("@?(.*/)") .. "quotes.json"
 
@@ -70,28 +59,56 @@ function M.start_test()
 	vim.bo.buftype = "nofile"
 	vim.bo.bufhidden = "wipe"
 	vim.wo.wrap = true
+	vim.wo.relativenumber = false
+	vim.wo.number = false
 
+	-- Set the quote text
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, { quote.text })
-	vim.cmd("startinsert")
 
-	-- Replace vim.Loop.New_timer with vim.Defer_fn
-	vim.defer_fn(function()
-		local input = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
-		local elapsed = os.difftime(os.time(), start_time)
+	-- Highlight groups
+	vim.cmd("highlight TypingCorrect guifg=green gui=bold")
+	vim.cmd("highlight TypingIncorrect guifg=red gui=bold")
 
-		local words = #vim.split(input, "%s+")
-		local correct_chars = 0
-		for i = 1, #quote.text do
-			if quote.text:sub(i, i) == input:sub(i, i) then
+	-- State management
+	local cursor_pos = 0
+	local correct_chars = 0
+	local start_time = os.time()
+
+	-- Attach a key handler to capture typing
+	vim.api.nvim_buf_attach(0, false, {
+		on_lines = function()
+			return true
+		end, -- Prevent line editing
+		on_key = function(_, key)
+			-- Get current character in the quote
+			local expected_char = quote.text:sub(cursor_pos + 1, cursor_pos + 1)
+
+			if key == expected_char then
+				-- Correct character
 				correct_chars = correct_chars + 1
+				vim.api.nvim_buf_add_highlight(0, -1, "TypingCorrect", 0, cursor_pos, cursor_pos + 1)
+			else
+				-- Incorrect character
+				vim.api.nvim_buf_add_highlight(0, -1, "TypingIncorrect", 0, cursor_pos, cursor_pos + 1)
 			end
-		end
 
-		local wpm = math.floor(words / (elapsed / 60))
-		local accuracy = math.floor((correct_chars / #quote.text) * 100)
+			-- Move cursor forward
+			cursor_pos = cursor_pos + 1
 
-		vim.notify(string.format("WPM: %d | Accuracy: %d%%", wpm, accuracy), vim.log.levels.INFO)
-		vim.cmd("stopinsert | q!")
-	end, M.config.test_duration * 1000)
+			-- End test if all characters are typed
+			if cursor_pos == #quote.text then
+				local elapsed = os.difftime(os.time(), start_time)
+				local wpm = math.floor((#vim.split(quote.text, "%s+") / (elapsed / 60)))
+				local accuracy = math.floor((correct_chars / #quote.text) * 100)
+
+				vim.notify(string.format("WPM: %d | Accuracy: %d%%", wpm, accuracy), vim.log.levels.INFO)
+				vim.cmd("stopinsert | q!")
+			end
+		end,
+	})
+
+	-- Start insert mode
+	vim.cmd("startinsert")
 end
+
 return M
