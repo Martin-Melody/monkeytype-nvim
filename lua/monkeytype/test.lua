bcd27918ede8d394569081ebd0c6366faaa766d8
Martin <martinmelody.work@gmail.com>
2024-11-25 19:57:34 +0000

    working on typing

--- lua/monkeytype/test.lua
+++ lua/monkeytype/test.lua
@@ -2,6 +2,7 @@ local M = {}
 local config = require("monkeytype.config")
 local utils = require("monkeytype.utils")
 
+-- Function to load quotes
 function M.load_quotes()
 	local user_file = config.config.user_quotes_file
 	local default_file = config.config.default_quotes_file
@@ -24,6 +25,7 @@ function M.load_quotes()
 	return {}
 end
 
+-- Function to start the typing test
 function M.start_test()
 	local quotes = M.load_quotes()
 	if #quotes == 0 then
@@ -39,23 +41,36 @@ function M.start_test()
 	vim.wo.relativenumber = false
 	vim.wo.number = false
 
-	-- Set the quote text
-	vim.api.nvim_buf_set_lines(0, 0, -1, false, { quote.text })
+	-- Set the quote as placeholder text
+	local placeholder = { quote.text }
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, placeholder)
 
 	-- Highlight groups
 	vim.cmd("highlight TypingCorrect guifg=green gui=bold")
 	vim.cmd("highlight TypingIncorrect guifg=red gui=bold")
+	vim.cmd("highlight TypingPlaceholder guifg=gray gui=italic")
+
+	-- Apply placeholder highlight
+	vim.api.nvim_buf_add_highlight(0, -1, "TypingPlaceholder", 0, 0, -1)
 
 	-- State management
+	local user_input = {}
 	local cursor_pos = 0
 	local correct_chars = 0
 	local start_time = os.time()
 
-	-- Function to handle typing
+	-- Function to handle input
 	local function handle_input(key)
-		-- Get the current character in the quote
+		-- Get the expected character at the current position
 		local expected_char = quote.text:sub(cursor_pos + 1, cursor_pos + 1)
 
+		-- Replace the placeholder with the user's input
+		if #key > 0 then
+			user_input[cursor_pos + 1] = key
+			vim.api.nvim_buf_set_text(0, 0, cursor_pos, 0, cursor_pos + 1, { key })
+		end
+
+		-- Highlight correct/incorrect characters
 		if key == expected_char then
 			correct_chars = correct_chars + 1
 			vim.api.nvim_buf_add_highlight(0, -1, "TypingCorrect", 0, cursor_pos, cursor_pos + 1)
@@ -63,7 +78,7 @@ function M.start_test()
 			vim.api.nvim_buf_add_highlight(0, -1, "TypingIncorrect", 0, cursor_pos, cursor_pos + 1)
 		end
 
-		-- Move the cursor forward
+		-- Move cursor forward
 		cursor_pos = cursor_pos + 1
 
 		-- End the test if all characters are typed
@@ -78,20 +93,6 @@ function M.start_test()
 	end
 
 	-- Map all keys to handle_input in insert mode
-	vim.api.nvim_buf_set_keymap(
-		0,
-		"i",
-		"<BS>",
-		"<Cmd>lua vim.notify('Backspace is not allowed!', vim.log.levels.WARN)<CR>",
-		{}
-	)
-	vim.api.nvim_buf_attach(0, false, {
-		on_lines = function()
-			return true
-		end, -- Prevent line editing
-	})
-
-	-- Redirect input to handle_input
 	vim.api.nvim_create_autocmd("InsertCharPre", {
 		buffer = 0,
 		callback = function(args)
