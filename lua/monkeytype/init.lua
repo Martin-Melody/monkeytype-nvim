9d52a8b141761559221a0099c7bd235b50df9a14
Martin <martinmelody.work@gmail.com>
2024-11-25 19:19:55 +0000

    commit

--- lua/monkeytype/init.lua
+++ lua/monkeytype/init.lua
@@ -14,7 +14,9 @@ M.default_quotes_file = debug.getinfo(1).source:match("@?(.*/)") .. "quotes.json
 
 -- Setup function
 function M.setup(user_config)
+	-- Deep merge user configuration with default configuration
 	M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
+	-- Set up keymaps
 	M.set_keymaps()
 end
 
@@ -22,11 +24,14 @@ end
 function M.set_keymaps()
 	for action, map in pairs(M.config.keymaps) do
 		if M[action] then -- Only set keymaps for existing functions
-			vim.keymap.set(map.mode, map.lhs, M[action], { desc = map.desc })
+			vim.keymap.set(map.mode, map.lhs, function()
+				M[action]()
+			end, { desc = map.desc })
 		end
 	end
 end
 
+-- Function to load quotes
 function M.load_quotes()
 	local quotes_file = M.config.quotes_file
 
@@ -47,10 +52,12 @@ function M.load_quotes()
 	end
 
 	local default_content = default_file:read("*a")
+
 	default_file:close()
 	return vim.json.decode(default_content)
 end
 
+-- Function to start the typing test
 function M.start_test()
 	local quotes = M.load_quotes()
 	if #quotes == 0 then
@@ -67,32 +74,24 @@ function M.start_test()
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, { quote.text })
 	vim.cmd("startinsert")
 
-	local start_time = os.time()
-	local timer = vim.loop.new_timer()
-	timer:start(
-		M.config.test_duration * 1000,
-		0,
-		vim.schedule_wrap(function()
-			local input = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
-			local elapsed = os.difftime(os.time(), start_time)
+	-- Replace vim.Loop.New_timer with vim.Defer_fn
+	vim.defer_fn(function()
+		local input = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
+		local elapsed = os.difftime(os.time(), start_time)
 
-			local words = #vim.split(input, "%s+")
-			local correct_chars = 0
-			for i = 1, #quote.text do
-				if quote.text:sub(i, i) == input:sub(i, i) then
-					correct_chars = correct_chars + 1
-				end
+		local words = #vim.split(input, "%s+")
+		local correct_chars = 0
+		for i = 1, #quote.text do
+			if quote.text:sub(i, i) == input:sub(i, i) then
+				correct_chars = correct_chars + 1
 			end
+		end
 
-			local wpm = math.floor(words / (elapsed / 60))
-			local accuracy = math.floor((correct_chars / #quote.text) * 100)
+		local wpm = math.floor(words / (elapsed / 60))
+		local accuracy = math.floor((correct_chars / #quote.text) * 100)
 
-			vim.notify(string.format("WPM: %d | Accuracy: %d%%", wpm, accuracy), vim.log.levels.INFO)
-			vim.cmd("stopinsert | q!")
-			timer:stop()
-			timer:close()
-		end)
-	)
+		vim.notify(string.format("WPM: %d | Accuracy: %d%%", wpm, accuracy), vim.log.levels.INFO)
+		vim.cmd("stopinsert | q!")
+	end, M.config.test_duration * 1000)
 end
-
 return M
