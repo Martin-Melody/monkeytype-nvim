388f708e13941fb1a65d9be8d71b520bb13291ec
Martin <martinmelody.work@gmail.com>
2024-11-25 19:44:13 +0000

    trying to fix read file

--- lua/monkeytype/utils.lua
+++ lua/monkeytype/utils.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.read_file(path)
 	local file = io.open(path, "r")
 	if not file then
+		print("File not found:", path) -- Debug message
 		return nil
 	end
 	local content = file:read("*a")
